7ee40d66a5494d0f55530bb5437d206de3950df0
nuradilzhanadiluulu <nuradilzhanadiluul21@gmail.com>
2022-05-23 02:44:14 -0500

    Creating class and TEXT file

---
+++ src/main/java/EstyWeddingParty/WeddingPartyAutomation.java
@@ -0,0 +1,12 @@
+package EstyWeddingParty;
+
+import org.testng.annotations.Test;
+
+public class WeddingPartyAutomation {
+    //you can start creating your automation codes here\
+    //just create a Test annotation and add you code inside that method
+    //I will create a BrowserUtils Class to create our first utils
+    //Do not forget to PULL and PUSH codes before and after finishing working
+    //I am going to add the steps for pulling and pushing in the TEXT file inside the NOTES package
+    //Sincerely Nuradil!
+}
