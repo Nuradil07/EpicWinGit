d7deb0970cedc85ff812c566964c237334baa2a0
Altynai Mahmud <samaltynay@gmail.com>
2022-05-23 20:07:52 -0500

    11

--- src/main/java/EstyWeddingParty/WeddingPartyAutomation.java
+++ src/main/java/EstyWeddingParty/WeddingPartyAutomation.java
@@ -1,5 +1,8 @@
 package EstyWeddingParty;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class WeddingPartyAutomation {
@@ -9,4 +12,17 @@ public class WeddingPartyAutomation {
     //Do not forget to PULL and PUSH codes before and after finishing working
     //I am going to add the steps for pulling and pushing in the TEXT file inside the NOTES package
     //Sincerely Nuradil!
+
+    @Test
+    public void EtsyPage(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.etsy.com/");
+
+
+    }
+
+
 }
